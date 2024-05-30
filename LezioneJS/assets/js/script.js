c4f2cb8d9ebdcd6d63057a165fad53dc6e0dfc85
Emanuele <info@emanueleumberto.it>
2024-05-30 13:46:16 +0200

    Intro Javascript

--- LezioneJS/assets/js/script.js
+++ LezioneJS/assets/js/script.js
@@ -89,6 +89,27 @@ if(a < b) {
     console.log('A è maggiore di B');
 }
 
+// Switch/Case
+/* 
+    switch (key) {
+        case value:
+            // Blocco di istruzioni
+            break;
+        case value:
+            // Blocco di istruzioni
+            break;
+        case value:
+            // Blocco di istruzioni
+            break;
+        case value:
+            // Blocco di istruzioni
+            break;
+        default:
+            // Blocco di istruzioni
+            break;
+    } 
+*/
+
 
 // Esercizio
 // Chiedi tramite un prompt un valore numerico da 1-7
@@ -96,3 +117,105 @@ if(a < b) {
 // Es. 1 = Lunedi, 2 = Martedi, 3 = Mercoledi ....
 // Se l'utente inserisce un valore diverso da 1-7 
 // stampare un messaggio di errore
+
+// Soluzione con if/else
+let giorno = Number(prompt('Inserisci un valore numerico da 1 a 7'));
+/* giorno = Number(giorno); */
+if(giorno === 1) { console.log('Lunedi');}
+else if(giorno === 2) { console.log('Martedi');}
+else if(giorno === 3) { console.log('Mercoledi');}
+else if(giorno === 4) { console.log('Giovedi');}
+else if(giorno === 5) { console.log('Venerdi');}
+else if(giorno === 6) { console.log('Sabato');}
+else if(giorno === 7) { console.log('Domenica');}
+else { console.log('Hai inserito un valore errato') }
+
+// Soluzione con swich/case
+switch (giorno) {
+    case 1: { console.log('Lunedi'); break; }
+    case 2: { console.log('Martedi'); break; }
+    case 3: { console.log('Mercoledi'); break; }
+    case 4: { console.log('Giovedi'); break; }
+    case 5: { console.log('Venerdi'); break; }
+    case 6: { console.log('Sabato'); break; }
+    case 7: { console.log('Domenica'); break; }
+    default:
+        console.log('Hai inserito un valore errato')
+        break;
+}
+
+let mese = Number(prompt('Inserisici un mese da 1 a 12'))
+switch (mese) {
+    case 12: 
+    case 1: 
+    case 2: { console.log('Inverno'); break; }
+    case 3:
+    case 4: 
+    case 5: { console.log('Primavera'); break; }
+    case 6: 
+    case 7: 
+    case 8: { console.log('Estate'); break; }
+    case 9: 
+    case 10: 
+    case 11: { console.log('Autunno'); break; }
+    default:
+        console.log('Hai inserito un valore errato')
+        break;
+}
+
+// Cicli o Strutture iterative
+// FOR | WHILE | DO WHILE
+// for(dichiarazione; condizione; incremento) { blocco di istruzioni }
+console.log('Ciclo FOR');
+for(let i=0; i<5; i++) {
+    // Blocco di istruzioni
+    console.log(i);
+}
+
+console.log('Ciclo WHILE');
+let i = 0;
+while(true) {
+    // Blocco di istruzioni
+    console.log(i);
+    i++;
+    let c = confirm('Vuoi Continiare?')
+    if(c === false) { break; }
+}
+
+console.log('Ciclo DO WHILE');
+let h = 10;
+do {
+    // Blocco di istruzioni
+    console.log(h);
+    h++;
+} while(h<5)
+
+
+
+// Esercizio
+// Chiedi con un ciclo n volte tramite prompt 
+// un nome,un cognome e un età ad un utente
+// Stampa in console i valori richiesti 
+// in questa forma "Ciao Mario Rossi anni 22" 
+// ma solo se l'età inserita è una età valida
+// e che sia maggiore di 18 anni altrimenti
+// stampare un messaggio di errore
+
+while(true) {
+    let nome = prompt('Iserisci il tuo nome')
+    let cognome = prompt('Iserisci il tuo cognome')
+    let eta = Number(prompt('Iserisci la tua età'))
+    if(eta > 0 && eta < 120) {
+        if(eta >= 18){
+            console.log("Ciao " + nome + " " + cognome + " anni " + eta);
+        } else {
+            console.log("Ciao " + nome + " " + cognome);
+        }
+    } else {
+        console.log("Errore!!! valori non corretti");
+    }
+    let continua = confirm('Vuoi continuare?')
+    if(continua === false) { break; }
+}
+console.log("Fine");
+
---
+++ LezioneJS/assets/js/script2.js
@@ -0,0 +1,133 @@
+// Number | String | Boolean | Array |
+let num = 25;
+let txt = 'testo';
+let bool = true; 
+
+// Array o Vettori
+let arr = [5, 63, 8, 99, 51]; // 0, 1, 2, 3, ...
+let x = arr[1];
+arr[2] = 10;
+arr[5] = 3;
+arr[7] = 31;
+
+arr.push(12); // Inserisce un nuovo valore alla fine dell'array
+arr.unshift(1); // Inserisce un nuovo valore all'inizio dell'array
+arr.pop(); // Rimuove un elemento alla fine di un array
+arr.shift(); // Rimuove un elemento all'inizio di un array
+
+console.log(arr);
+// Può essere utilizzato per 
+// eliminare/sostituire/aggiungere 
+// un elemento ad un indice specifico
+arr.splice(4, 1, 55) 
+console.log(arr);
+
+console.log(arr.length);
+console.log(x);
+
+// Ciclo for utilizzato per iterare un array tramite l'indice
+for(let i=0; i<arr.length; i++) {
+    console.log("Il valore contenuto all'indice " + 
+                i + " nell'array è: " + arr[i]);
+}
+
+// ForIN | FOROF
+// ForIN utilizzato solo per iterare completamente un array
+// mi restituisce in ogni iterazione l'indice dell'array
+for (const key in arr) {
+    console.log("Il valore contenuto all'indice " + 
+                key + " nell'array è: " + arr[key]);
+}
+
+// ForOF utilizzato solo per iterare completamente un array
+// mi restituisce in ogni iterazione il valore contenuto nell'array
+for (const value of arr) {
+    console.log("Il valore contenuto nell'array è: " + value);
+}
+
+// ESERCIZIO
+// Chiedere ad un utente tramite un prompt 
+// 10 numeri validi ed univoci. 
+// Al termine stamparli in console 
+
+let arrNum = [];
+
+/* let n = prompt('Iserisci un numero');
+let numVal = Number(n);
+console.log(numVal);   */
+// typeof restituisce il tipo di dato di una variabile
+//console.log(typeof(numVal));
+// NaN è un valore numerico che non è comparabile neanche con se stesso
+// isNaN è una funzione che restituisce true se la variabile contiene un valore NaN
+//console.log(isNaN(numVal));
+
+while(true) {
+    let num = prompt('Iserisci un numero');
+    let numVal = Number(num); // numero | NaN
+    if(!isNaN(numVal)) {
+        /* 
+            let n = numVal;
+            for (const value of arrNum) {
+                if(value === numVal) { n = null; }
+            } 
+            
+            if(n !== null) { arrNum.push(numVal) }
+        */
+        
+        if(!arrNum.includes(numVal)) { arrNum.push(numVal) }
+    }
+    if(arrNum.length === 1) { break; }
+}
+
+console.log(arrNum);
+
+/* for(let i=0; i<arrNum.length; ) {
+    let num = prompt('Iserisci un numero');
+    arrNum.push(num)
+    i++;
+} */
+
+
+
+// Object
+
+let utente = { nome: "Mario", cognome: "Rossi", eta: 43 }
+utente.nome = "Giuseppe";
+console.log(utente.nome);
+console.log(utente.cognome);
+console.log(utente['eta']);
+
+
+// ESERCIZIO
+// Chiedere ad un utente tramite un prompt 
+// 5 utenti che hanno le seguenti proprietà.
+// Nome, Cognome, Eta, Email
+// Salvare gli oggetti di tipo Utente in un array 
+// Al termine stamparli in console 
+// EXTRA: controllo sulla email (duplicata)
+
+let arrUser = [];
+
+while (arrUser.length < 3) {
+    let nome = prompt('Inserisci il tuo nome');
+    let cognome = prompt('Inserisci il tuo cognome');
+    let eta = prompt('Inserisci la tua eta');
+    let email = prompt('Inserisci la tua email');
+
+    // Controllo se l'indirizzo email è già presente
+    let n = email;
+    for (const obj of arrUser) {
+        if(obj.email === email) { n = null; }
+    } 
+    if(n !== null) { 
+        let obj = {
+            nome: nome, 
+            cognome: cognome, 
+            eta: eta, 
+            email: email
+        };
+        arrUser.push(obj);
+    }
+}
+
+console.log(arrUser);
